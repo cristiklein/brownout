dea31255fd5ba960a9546cb3da76decb20d6dd35
margueri <margueri@f64c6a06-03e8-4015-b226-b49493c48095>
2002-10-21 11:48:33 +0000

    *** empty log message ***
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/rubis/trunk@68 f64c6a06-03e8-4015-b226-b49493c48095

--- MDB/jonas_start.sh
+++ MDB/jonas_start.sh
@@ -2,6 +2,7 @@
 
 XTRA_CLASSPATH=.:`pwd`/edu/rice/rubis:`pwd`/edu/rice/rubis/servlets
 export XTRA_CLASSPATH
+export J2SE_PREEMPTCLOSE=1
 
 #registry &
 EJBServer &
--- MDB/tomcat_start.sh
+++ MDB/tomcat_start.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 . $JONAS_ROOT/bin/unix/config_env
-
-# classpath for JOnAS
 CLASSPATH=${CLASSPATH}:.:`pwd`/rubis.jar:`pwd`/rubis_ejb_servlets.jar
-
-# classpath for JBoss 3.0
-#CLASSPATH=${CLASSPATH}:.:`pwd`/rubis.jar:`pwd`/rubis_ejb_servlets.jar:/usr/java/jboss-3.0.0/client/jnet.jar:/usr/java/jboss-3.0.0/client/jboss-j2ee.jar:/usr/java/jboss-3.0.0/client/jboss-client.jar:/usr/java/jboss-3.0.0/client/jbosssx-client.jar:/usr/java/jboss-3.0.0/client/jnp-client.jar:/usr/java/jboss-3.0.0/client/jboss-common-client.jar:/usr/java/jboss-3.0.0/client/log4j.jar:/usr/java/jboss-3.0.0/client/jbossmq-client.jar:/usr/java/jboss-3.0.0/client/concurrent.jar
 export CLASSPATH
+
+### JBoss + RMI ###
+#export TOMCAT_OPTS="-Xmx512m -Xss16k -Djava.naming.factory.initial=com.sun.jndi.rmi.registry.RegistryContextFactory -Djava.naming.provider.url=rmi://localhost:1099 -Djava.naming.factory.url.pkgs=org.jboss.naming"
 
 ### JBoss + JNP ###
 #export TOMCAT_OPTS="-Xmx512m -Xss16k -Djava.naming.factory.initial=org.jnp.interfaces.NamingContextFactory -Djava.naming.provider.url=localhost -Djava.naming.factory.url.pkgs=org.jboss.naming:org.jnp.interfaces"
