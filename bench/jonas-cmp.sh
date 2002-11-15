f570d4f0dcfb468ba8b55b21330c09ec98232c86
margueri <margueri@f64c6a06-03e8-4015-b226-b49493c48095>
2002-11-15 13:45:15 +0000

    - replace rsh with ssh
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/rubis/trunk@73 f64c6a06-03e8-4015-b226-b49493c48095

--- bench/jonas-cmp.sh
+++ bench/jonas-cmp.sh
@@ -1,6 +1,6 @@
 #!/bin/tcsh
 
-setenv EJBDIR /users/cecchet/RUBiS/EJB_EntityBean_id
+setenv EJBDIR /users/margueri/RUBiS/EJB_EntityBean_id
 
 # Go back to RUBiS root directory
 cd ..
@@ -9,21 +9,23 @@ cd ..
 
 cp ./workload/browse_only_transitions_7.txt ./workload/transitions.txt
 
-foreach i ( rubis.properties_20 rubis.properties_40 rubis.properties_60 rubis.properties_80 rubis.properties_100 rubis.properties_120 rubis.properties_140 rubis.properties_160 rubis.properties_180 rubis.properties_200 rubis.properties_220 rubis.properties_240 rubis.properties_260 rubis.properties_280 rubis.properties_300)
+# rubis.properties_20 rubis.properties_40 rubis.properties_60 rubis.properties_80 rubis.properties_100 rubis.properties_120 rubis.properties_140 rubis.properties_160 rubis.properties_180 rubis.properties_200 rubis.properties_220 rubis.properties_240 
+
+foreach i ( rubis.properties_260 rubis.properties_280 rubis.properties_300)
   cp bench/$i Client/rubis.properties
-  rsh sci21 -n -l root ${EJBDIR}/tomcat_stop.sh 
-  rsh sci20 ${EJBDIR}/jonas_stop.sh
+  ssh sci21 -n -l root ${EJBDIR}/tomcat_stop.sh 
+  ssh sci20 ${EJBDIR}/jonas_stop.sh
   sleep 4
-  rsh sci22 ${EJBDIR}/update_ids.sh
-  rsh sci21 -n -l root ${EJBDIR}/tomcat_jonas_start.sh &
-  rsh sci20 "cd ${EJBDIR} ; jonas_start.sh" &
+  ssh sci22 ${EJBDIR}/update_ids.sh
+  ssh sci21 -n -l root ${EJBDIR}/tomcat_jonas_start.sh &
+  ssh sci20 "cd ${EJBDIR} ; jonas_start.sh" &
   sleep 4
-  bench/flush_cache 490000
-  rsh sci8 RUBiS/bench/flush_cache 490000	# remote client
-  rsh sci20 RUBiS/bench/flush_cache 880000      # ejb server
-  rsh sci21 RUBiS/bench/flush_cache 880000 	# servlet server
-  rsh sci23 RUBiS/bench/flush_cache 880000 	# web server
-  rsh sci22 RUBiS/bench/flush_cache 880000	# database
+  bench/flush_cache 190000
+  ssh sci31 RUBiS/bench/flush_cache 190000         # remote client
+  ssh sci20 RUBiS/bench/flush_cache 880000      # ejb server
+  ssh sci21 RUBiS/bench/flush_cache 880000 	# servlet server
+  ssh sci23 RUBiS/bench/flush_cache 880000 	# web server
+  ssh sci22 RUBiS/bench/flush_cache 880000	# database
   make emulator
 end
 
@@ -31,24 +33,25 @@ end
 
 cp ./workload/default_transitions_7.txt ./workload/transitions.txt
 
+#rubis.properties_20 rubis.properties_40 rubis.properties_60 rubis.properties_80 rubis.properties_100 rubis.properties_120 rubis.properties_140 rubis.properties_160 rubis.properties_180 
 
-foreach i ( rubis.properties_20 rubis.properties_40 rubis.properties_60 rubis.properties_80 rubis.properties_100 rubis.properties_120 rubis.properties_140 rubis.properties_160 rubis.properties_180 rubis.properties_200 rubis.properties_220 rubis.properties_240 rubis.properties_260 rubis.properties_280 rubis.properties_300)
+foreach i (  rubis.properties_160 rubis.properties_180 rubis.properties_200 rubis.properties_220 rubis.properties_240 rubis.properties_260 rubis.properties_280 rubis.properties_300)
   cp bench/$i Client/rubis.properties
-  rsh sci21 -n -l root ${EJBDIR}/tomcat_stop.sh 
-  rsh sci20 ${EJBDIR}/jonas_stop.sh
+  ssh sci21 -n -l root ${EJBDIR}/tomcat_stop.sh 
+  ssh sci20 ${EJBDIR}/jonas_stop.sh
   sleep 4
-  rsh sci22 ${EJBDIR}/update_ids.sh
-  rsh sci21 -n -l root ${EJBDIR}/tomcat_jonas_start.sh &
-  rsh sci20 "cd ${EJBDIR} ; jonas_start.sh" &
+  ssh sci22 ${EJBDIR}/update_ids.sh
+  ssh sci21 -n -l root ${EJBDIR}/tomcat_jonas_start.sh &
+  ssh sci20 "cd ${EJBDIR} ; jonas_start.sh" &
   sleep 4
-  bench/flush_cache 490000
-  rsh sci8 RUBiS/bench/flush_cache 490000	# remote client
-  rsh sci20 RUBiS/bench/flush_cache 880000      # ejb server
-  rsh sci21 RUBiS/bench/flush_cache 880000 	# servlet server
-  rsh sci23 RUBiS/bench/flush_cache 880000 	# web server
-  rsh sci22 RUBiS/bench/flush_cache 880000	# database
+  bench/flush_cache 190000
+  ssh sci31 RUBiS/bench/flush_cache 190000	# remote client
+  ssh sci20 RUBiS/bench/flush_cache 880000      # ejb server
+  ssh sci21 RUBiS/bench/flush_cache 880000 	# servlet server
+  ssh sci23 RUBiS/bench/flush_cache 880000 	# web server
+  ssh sci22 RUBiS/bench/flush_cache 880000	# database
   make emulator
 end
 
-rsh sci20 ${EJBDIR}/jonas_stop.sh
+ssh sci20 ${EJBDIR}/jonas_stop.sh
 sleep 4
--- bench/jonas_localremote_prof.sh
+++ bench/jonas_localremote_prof.sh
@@ -8,7 +8,7 @@ cd ..
 cp ./workload/default_transitions_7.txt ./workload/transitions.txt
 
 
-setenv EJBDIR /users/cecchet/RUBiS/EJB_local_remote
+setenv EJBDIR /users/margueri/RUBiS/EJB_local_remote
 foreach i (  rubis.properties_jonas_rmi_lr_prof)
   cp bench/$i Client/rubis.properties
   rsh sci21 -n -l root ${EJBDIR}/tomcat_stop.sh 
@@ -18,8 +18,8 @@ foreach i (  rubis.properties_jonas_rmi_lr_prof)
   rsh sci21 -n -l root ${EJBDIR}/tomcat_jonas_start.sh &
   rsh sci20 "cd ${EJBDIR} ; jonas_profile_start.sh" &
   sleep 10
-  bench/flush_cache 490000
-  rsh sci8 RUBiS/bench/flush_cache 490000	# remote client
+  bench/flush_cache 190000
+  rsh sci31 RUBiS/bench/flush_cache 190000	# remote client
   rsh sci20 RUBiS/bench/flush_cache 880000      # ejb server
   rsh sci21 RUBiS/bench/flush_cache 880000 	# servlet server
   rsh sci23 RUBiS/bench/flush_cache 880000 	# web server
