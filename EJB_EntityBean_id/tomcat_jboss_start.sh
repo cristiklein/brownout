ee6f17f57c7e625ae2f26bffe2c394def4fe8fd5
margueri <margueri@f64c6a06-03e8-4015-b226-b49493c48095>
2002-12-06 13:56:58 +0000

    added missing scripts
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/rubis/trunk@117 f64c6a06-03e8-4015-b226-b49493c48095

---
+++ EJB_EntityBean_id/tomcat_jboss_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CLASSPATH=${CLASSPATH}:/users/margueri/RUBiS/EJB_EntityBean_id/rubis.jar:/users/margueri/RUBiS/EJB_EntityBean_id/rubis_ejb_servlets.jar:${JBOSS_DIST}/client/jnet.jar:${JBOSS_DIST}/client/jboss-j2ee.jar:${JBOSS_DIST}/client/jboss-client.jar:${JBOSS_DIST}/client/jbosssx-client.jar:${JBOSS_DIST}/client/jnp-client.jar:${JBOSS_DIST}/client/jboss-common-client.jar:${JBOSS_DIST}/client/log4j.jar:${JBOSS_DIST}/client/jbossmq-client.jar:${JBOSS_DIST}/client/concurrent.jar
+
+export CLASSPATH
+
+### JBoss + JNP ###
+export TOMCAT_OPTS="-Xmx512m -Xss96k -Djava.naming.factory.initial=org.jnp.interfaces.NamingContextFactory -Djava.naming.provider.url=sci20 -Djava.naming.factory.url.pkgs=org.jboss.naming:org.jnp.interfaces"
+
+/opt/jakarta-tomcat-3.2.3/bin/startup.sh
---
+++ EJB_EntityBean_id/tomcat_jonas_start.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+. $JONAS_ROOT/bin/unix/config_env
+
+# classpath for jonas2.6 rmi
+#CLASSPATH=${CLASSPATH}:.:/users/margueri/RUBiS/EJB_EntityBean_id/rubis.jar:/users/margueri/RUBiS/EJB_EntityBean_id/rubis_ejb_servlets.jar:${J2EE_HOME}/lib/j2ee.jar:${JONAS_ROOT}/lib/RMI_client.jar:${JONAS_ROOT}/lib/RMI_jonas.jar
+
+# classpath for jonas2.6 jeremie
+CLASSPATH=${CLASSPATH}:.:/users/margueri/RUBiS/EJB_EntityBean_id/rubis.jar:/users/margueri/RUBiS/EJB_EntityBean_id/rubis_ejb_servlets.jar:${J2EE_HOME}/lib/j2ee.jar:${JONAS_ROOT}/lib/JEREMIE_client.jar:${JONAS_ROOT}/lib/JEREMIE_jonas.jar:${JONAS_ROOT}/config
+
+# classpath for jonas2.5
+CLASSPATH=${CLASSPATH}:.:/users/margueri/RUBiS/EJB_EntityBean_id/rubis.jar:/users/margueri/RUBiS/EJB_EntityBean_id/rubis_ejb_servlets.jar:${J2EE_HOME}/lib/j2ee.jar
+
+export CLASSPATH
+
+### Jonas + rmi ###
+#export TOMCAT_OPTS="-Xmx512m -Xss96k -Djava.naming.factory.initial=com.sun.jndi.rmi.registry.RegistryContextFactory -Djava.naming.provider.url=rmi://sci20:1099 -Djava.naming.factory.url.pkgs=org.objectweb.jonas.naming"
+
+
+
+### Jonas + jeremie ###
+export TOMCAT_OPTS="-Xmx512m -Xss96k -Djava.naming.factory.initial=org.objectweb.jeremie.libs.services.registry.jndi.JRMIInitialContextFactory -Djava.naming.provider.url=jrmi://sci20:12340 -Djava.naming.factory.url.pkgs=org.objectweb.jonas.naming"
+
+/opt/jakarta-tomcat-3.2.3/bin/startup.sh
