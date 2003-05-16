74ead6cab0697c0e7224e890319b8f0bb26b913c
margueri <margueri@f64c6a06-03e8-4015-b226-b49493c48095>
2003-05-16 14:39:45 +0000

    Updated for jonas 3.x
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/rubis/trunk@182 f64c6a06-03e8-4015-b226-b49493c48095

--- EJB_local_remote/jonas_start.sh
+++ EJB_local_remote/jonas_start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-XTRA_CLASSPATH=.:`pwd`/edu/rice/rubis:`pwd`/edu/rice/rubis/servlets
-export XTRA_CLASSPATH
-
 # JVM options for Jrockit
 #JAVA_OPTS="-Xnoopt -Xms128m -Xmx778m -Xss64k"
 
@@ -14,6 +11,8 @@ JAVA_OPTS="-Xms128m -Xmx768m -Xss16k"
 export JAVA_OPTS
 
 # jonas<2.6
+#XTRA_CLASSPATH=.:`pwd`/edu/rice/rubis:`pwd`/edu/rice/rubis/servlets
+#export XTRA_CLASSPATH
 #EJBServer &
 
 # since jonas 2.6
