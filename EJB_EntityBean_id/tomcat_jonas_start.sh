b86e381ef43c92d5fdf285551ebc293e62c0605c
margueri <margueri@f64c6a06-03e8-4015-b226-b49493c48095>
2003-05-16 14:20:32 +0000

    Fixed jonas_root
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/rubis/trunk@176 f64c6a06-03e8-4015-b226-b49493c48095

--- EJB_EntityBean_id/tomcat_jonas_start.sh
+++ EJB_EntityBean_id/tomcat_jonas_start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # classpath jonas 2.6/ 3.x rmi
-#CLASSPATH="$JAVA_HOME"/lib/tools.jar:"$JONAS_ROOT"/lib/RMI_client.jar:"$JONAS_ROOT"/lib/RMI_jonas.jar:/users/margueri/RUBiS/EJB_EntityBean_id/rubis.jar
+#CLASSPATH=${JONAS_ROOT}/lib/RMI_client.jar:${JONAS_ROOT}/lib/RMI_jonas.jar:/users/margueri/RUBiS/EJB_EntityBean_id/rubis.jar
 
 # classpath jonas 2.6/ 3.x jeremie
-#CLASSPATH="$JAVA_HOME"/lib/tools.jar:"$JONAS_ROOT"/lib/JEREMIE_client.jar:"$JONAS_ROOT"/lib/JEREMIE_jonas.jar:/users/margueri/RUBiS/EJB_EntityBean_id/rubis.jar
+#CLASSPATH=${JONAS_ROOT}/lib/JEREMIE_client.jar:${JONAS_ROOT}/lib/JEREMIE_jonas.jar:/users/margueri/RUBiS/EJB_EntityBean_id/rubis.jar
 
 # classpath jonas 3.1 with carol
-#CLASSPATH="$JAVA_HOME"/lib/tools.jar:"$JONAS_ROOT"/lib/client.jar:"$JONAS_ROOT"/lib/jonas.jar:"$JONAS_ROOT"/lib/common/carol/carol.jar:/users/margueri/RUBiS/EJB_EntityBean_id/rubis.jar
+#CLASSPATH=${JONAS_ROOT}/lib/client.jar:${JONAS_ROOT}/lib/jonas.jar:${JONAS_ROOT}/lib/common/carol/carol.jar:/users/margueri/RUBiS/EJB_EntityBean_id/rubis.jar
 
 # classpath for jonas2.5
 #. $JONAS_ROOT/bin/unix/config_env
