a2504f0539f9f066223528b6864325109989270e
margueri <margueri@f64c6a06-03e8-4015-b226-b49493c48095>
2003-05-16 14:33:51 +0000

    Updated for jonas 3.x
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/rubis/trunk@180 f64c6a06-03e8-4015-b226-b49493c48095

--- EJB_Session_facade/jonas_start.sh
+++ EJB_Session_facade/jonas_start.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-XTRA_CLASSPATH=.:`pwd`/edu/rice/rubis:`pwd`/edu/rice/rubis/servlets
-export XTRA_CLASSPATH
-JAVA_OPTS="-Xms128m -Xmx768m -Xss16k"
-export JAVA_OPTS
+#JAVA_OPTS="-Xms128m -Xmx768m -Xss16k"
+#export JAVA_OPTS
 
-#registry &
-EJBServer &
+# jonas<2.6
+#XTRA_CLASSPATH=.:`pwd`/edu/rice/rubis:`pwd`/edu/rice/rubis/servlets
+#export XTRA_CLASSPATH
+#EJBServer &
+
+# since jonas 2.6
+jonas start
