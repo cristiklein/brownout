dae4688c07158aa68ca19d40b7005f3b161c6398
margueri <margueri@f64c6a06-03e8-4015-b226-b49493c48095>
2003-05-19 13:16:08 +0000

    Updated for tomcat 4.1.24
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/rubis/trunk@185 f64c6a06-03e8-4015-b226-b49493c48095

--- Servlets/tomcat_start.sh
+++ Servlets/tomcat_start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-CLASSPATH=${CLASSPATH}:.:/users/margueri/RUBiS/Servlets/rubis_servlets.jar:/usr/local/java/mysql/mm.mysql-2.0.4-bin.jar
+CLASSPATH=/usr/local/mysql/mm.mysql-2.0.14-bin.jar
 export CLASSPATH
 
-/opt/jakarta-tomcat-3.2.3/bin/startup.sh
+#/opt/jakarta-tomcat-3.2.3/bin/startup.sh
+/opt/jakarta-tomcat-4.1.24/bin/startup.sh
