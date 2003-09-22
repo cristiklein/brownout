7cfbb8f5b58c5e91fd0152c63080f1a0d9007cb1
margueri <margueri@f64c6a06-03e8-4015-b226-b49493c48095>
2003-09-22 10:31:27 +0000

    Added some comments on how to use the script.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/rubis/trunk@235 f64c6a06-03e8-4015-b226-b49493c48095

--- bench/jboss-bmp.sh
+++ bench/jboss-bmp.sh
@@ -1,6 +1,23 @@
 #!/bin/tcsh
 
-setenv EJBDIR /users/cecchet/RUBiS/EJB_EntityBean_id_BMP
+###############################################################################
+#
+# This script runs first the RUBiS browsing mix, then the bidding mix 
+# for each rubis.properties_XX specified where XX is the number of emulated
+# clients. Note that the rubis.properties_XX files must be configured
+# with the corresponding number of clients.
+# In particular set the following variables in rubis.properties_XX:
+# httpd_use_version = EJB
+# workload_number_of_clients_per_node = XX/number of client machines
+# workload_transition_table = yourPath/RUBiS/workload/transitions.txt 
+#
+# This script should be run from the RUBiS/bench directory on the local 
+# client machine. 
+# Results will be generated in the RUBiS/bench directory.
+#
+################################################################################
+
+setenv EJBDIR /users/margueri/RUBiS/EJB_EntityBean_id_BMP
 
 # Go back to RUBiS root directory
 cd ..
@@ -10,20 +27,20 @@ cd ..
 cp ./workload/browse_only_transitions_7.txt ./workload/transitions.txt
 
 foreach i ( rubis.properties_20 rubis.properties_40 rubis.properties_60 rubis.properties_80 rubis.properties_100 rubis.properties_120 rubis.properties_140 rubis.properties_160 rubis.properties_180 rubis.properties_200 rubis.properties_220 rubis.properties_240 rubis.properties_260 rubis.properties_280 rubis.properties_300)
-  cp bench/$i Client/rubis.properties
-  rsh sci21 -n -l root ${EJBDIR}/tomcat_stop.sh
-  rsh sci20 ${EJBDIR}/jboss_stop.sh
+  cp bench/$i Client/build/rubis.properties
+  ssh sci21 -n -l margueri ${EJBDIR}/tomcat_stop.sh
+  ssh sci20 ${EJBDIR}/jboss_stop.sh
   sleep 4
-  rsh sci22 ${EJBDIR}/update_ids.sh
-  rsh sci21 -n -l root ${EJBDIR}/tomcat_jboss_start.sh &
-  rsh sci20 "cd ${EJBDIR} ; jboss_start.sh" &
+  ssh sci22 ${EJBDIR}/update_ids.sh
+  ssh sci21 -n -l margueri ${EJBDIR}/tomcat_jboss_start.sh &
+  ssh sci20 "cd ${EJBDIR} ; jboss_start.sh" &
   sleep 10
   bench/flush_cache 490000
-  rsh sci8 RUBiS/bench/flush_cache 490000	# remote client
-  rsh sci20 RUBiS/bench/flush_cache 880000      # ejb server
-  rsh sci21 RUBiS/bench/flush_cache 880000 	# servlet server
-  rsh sci23 RUBiS/bench/flush_cache 880000 	# web server
-  rsh sci22 RUBiS/bench/flush_cache 880000	# database
+  ssh sci8 RUBiS/bench/flush_cache 490000	# remote client
+  ssh sci20 RUBiS/bench/flush_cache 880000  # ejb server
+  ssh sci21 RUBiS/bench/flush_cache 880000 	# servlet server
+  ssh sci23 RUBiS/bench/flush_cache 880000 	# web server
+  ssh sci22 RUBiS/bench/flush_cache 880000	# database
   make emulator
 end
 
@@ -33,23 +50,22 @@ cp ./workload/default_transitions_7.txt ./workload/transitions.txt
 
 
 foreach i ( rubis.properties_20 rubis.properties_40 rubis.properties_60 rubis.properties_80 rubis.properties_100 rubis.properties_120 rubis.properties_140 rubis.properties_160 rubis.properties_180 rubis.properties_200 rubis.properties_220 rubis.properties_240 rubis.properties_260 rubis.properties_280 rubis.properties_300)
-  cp bench/$i Client/rubis.properties
-  cp bench/$i Client/rubis.properties
-  rsh sci21 -n -l root ${EJBDIR}/tomcat_stop.sh
-  rsh sci20 ${EJBDIR}/jboss_stop.sh
+  cp bench/$i Client/build/rubis.properties
+  ssh sci21 -n -l margueri ${EJBDIR}/tomcat_stop.sh
+  ssh sci20 ${EJBDIR}/jboss_stop.sh
   sleep 4
-  rsh sci22 ${EJBDIR}/update_ids.sh
-  rsh sci21 -n -l root ${EJBDIR}/tomcat_jboss_start.sh &
-  rsh sci20 "cd ${EJBDIR} ; jboss_start.sh" &
+  ssh sci22 ${EJBDIR}/update_ids.sh
+  ssh sci21 -n -l margueri ${EJBDIR}/tomcat_jboss_start.sh &
+  ssh sci20 "cd ${EJBDIR} ; jboss_start.sh" &
   sleep 10
   bench/flush_cache 490000
-  rsh sci8 RUBiS/bench/flush_cache 490000	# remote client
-  rsh sci20 RUBiS/bench/flush_cache 880000      # ejb server
-  rsh sci21 RUBiS/bench/flush_cache 880000 	# servlet server
-  rsh sci23 RUBiS/bench/flush_cache 880000 	# web server
-  rsh sci22 RUBiS/bench/flush_cache 880000	# database
+  ssh sci8 RUBiS/bench/flush_cache 490000	# remote client
+  ssh sci20 RUBiS/bench/flush_cache 880000  # ejb server
+  ssh sci21 RUBiS/bench/flush_cache 880000 	# servlet server
+  ssh sci23 RUBiS/bench/flush_cache 880000 	# web server
+  ssh sci22 RUBiS/bench/flush_cache 880000	# database
   make emulator
 end
 
-rsh sci20 ${EJBDIR}/jonas_stop.sh
+ssh sci20 ${EJBDIR}/jonas_stop.sh
 sleep 4
--- bench/jboss-sf.sh
+++ bench/jboss-sf.sh
@@ -1,6 +1,23 @@
 #!/bin/tcsh
 
-setenv EJBDIR /users/cecchet/RUBiS/EJB_Session_facade
+###############################################################################
+#
+# This script runs first the RUBiS browsing mix, then the bidding mix 
+# for each rubis.properties_XX specified where XX is the number of emulated
+# clients. Note that the rubis.properties_XX files must be configured
+# with the corresponding number of clients.
+# In particular set the following variables in rubis.properties_XX:
+# httpd_use_version = EJB
+# workload_number_of_clients_per_node = XX/number of client machines
+# workload_transition_table = yourPath/RUBiS/workload/transitions.txt 
+#
+# This script should be run from the RUBiS/bench directory on the local 
+# client machine. 
+# Results will be generated in the RUBiS/bench directory.
+#
+################################################################################
+
+setenv EJBDIR /users/margueri/RUBiS/EJB_Session_facade
 
 # Go back to RUBiS root directory
 cd ..
@@ -10,12 +27,12 @@ cd ..
 cp ./workload/browse_only_transitions_7.txt ./workload/transitions.txt
 
 foreach i ( rubis.properties_20 rubis.properties_40 rubis.properties_60 rubis.properties_80 rubis.properties_100 rubis.properties_120 rubis.properties_140 rubis.properties_160 rubis.properties_180 rubis.properties_200 rubis.properties_220 rubis.properties_240 rubis.properties_260 rubis.properties_280 rubis.properties_300 rubis.properties_320 rubis.properties_340 rubis.properties_360 rubis.properties_380 rubis.properties_400 rubis.properties_420 rubis.properties_440 rubis.properties_460 rubis.properties_480 rubis.properties_500) 
-  cp bench/$i Client/rubis.properties
-  rsh sci21 -n -l root ${EJBDIR}/tomcat_stop.sh
+  cp bench/$i Client/build/rubis.properties
+  rsh sci21 -n -l margueri ${EJBDIR}/tomcat_stop.sh
   rsh sci20 ${EJBDIR}/jboss_stop.sh
   sleep 4
   rsh sci22 ${EJBDIR}/update_ids.sh
-  rsh sci21 -n -l root ${EJBDIR}/tomcat_jboss_start.sh &
+  rsh sci21 -n -l margueri ${EJBDIR}/tomcat_jboss_start.sh &
   rsh sci20 "cd ${EJBDIR} ; jboss_start.sh" &
   sleep 10
   bench/flush_cache 490000
@@ -27,17 +44,17 @@ foreach i ( rubis.properties_20 rubis.properties_40 rubis.properties_60 rubis.pr
   make emulator
 end
 
-# Default JBoss
+# Bidding mix JBoss
 
 cp ./workload/default_transitions_7.txt ./workload/transitions.txt
 
 foreach i ( rubis.properties_20 rubis.properties_40 rubis.properties_60 rubis.properties_80 rubis.properties_100 rubis.properties_120 rubis.properties_140 rubis.properties_160 rubis.properties_180 rubis.properties_200 rubis.properties_220 rubis.properties_240 rubis.properties_260 rubis.properties_280 rubis.properties_300 rubis.properties_320 rubis.properties_340 rubis.properties_360 rubis.properties_380 rubis.properties_400 rubis.properties_420 rubis.properties_440 rubis.properties_460 rubis.properties_480 rubis.properties_500)
-  cp bench/$i Client/rubis.properties
-  rsh sci21 -n -l root ${EJBDIR}/tomcat_stop.sh
+  cp bench/$i Client/build/rubis.properties
+  rsh sci21 -n -l margueri ${EJBDIR}/tomcat_stop.sh
   rsh sci20 ${EJBDIR}/jboss_stop.sh
   sleep 4
   rsh sci22 ${EJBDIR}/update_ids.sh
-  rsh sci21 -n -l root ${EJBDIR}/tomcat_jboss_start.sh &
+  rsh sci21 -n -l margueri ${EJBDIR}/tomcat_jboss_start.sh &
   rsh sci20 "cd ${EJBDIR} ; jboss_start.sh" &
   sleep 10
   bench/flush_cache 490000
