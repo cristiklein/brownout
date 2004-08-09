950995554aec34ed59f8ff6ed40ea04b40b30ea6
margueri <margueri@f64c6a06-03e8-4015-b226-b49493c48095>
2004-08-09 09:58:25 +0000

    Added licence header
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/rubis/trunk@331 f64c6a06-03e8-4015-b226-b49493c48095

--- Client/edu/rice/rubis/beans/TimeManagement.java
+++ Client/edu/rice/rubis/beans/TimeManagement.java
@@ -1,4 +1,27 @@
-package edu.rice.rubis.beans;
+/*
+ * RUBiS
+ * Copyright (C) 2002, 2003, 2004 French National Institute For Research In Computer
+ * Science And Control (INRIA).
+ * Contact: jmob@objectweb.org
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or any later
+ * version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * Initial developer(s): Emmanuel Cecchet, Julie Marguerite
+ * Contributor(s): 
+ */
+ package edu.rice.rubis.beans;
 
 import java.util.GregorianCalendar;
 import java.lang.String;
